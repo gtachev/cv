7fbb1de377a02c7f89e4600b60a5e65f2bf1c77b
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2017-11-23 17:07:37 +0200

    Change license and add banner information to the bundles

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,12 @@ import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import { sync as inlineSync } from "inline-source";
 import { minify } from "html-minifier";
+import license from "rollup-plugin-license";
+const fs = require("fs");
 
 var bundleExampleHtml = {
     name: "html-bundler",
     onwrite: function() {
-        var fs = require("fs");
         var html = inlineSync("./example/index.html", {
             attribute: false,
             compress: false,
@@ -26,6 +27,18 @@ var bundleExampleHtml = {
     },
 };
 
+const licenseOptions = {
+    banner: `\
+Bundle of <%= pkg.name %> by <%= pkg.author.name %>
+Generated: <%= moment().format('YYYY-MM-DD') %>
+Version: <%= pkg.version %>
+License: <%= pkg.license %>
+Used dependencies:
+<% _.forEach(dependencies, function (dependency) { %>
+   <%= dependency.name %> <%= dependency.version %> by <%= dependency.author.name %> with <%= dependency.license %> license \
+<% }) %>`,
+};
+
 export default {
     input: "src/main.js",
     output: {
@@ -34,5 +47,5 @@ export default {
         name: "cv",
     },
     sourcemap: true,
-    plugins: [resolve(), babel(), bundleExampleHtml],
+    plugins: [resolve(), license(licenseOptions), babel(), bundleExampleHtml],
 };
