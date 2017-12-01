2009c2dd49f9405c50cf371efff99d7397e31e36
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2017-12-01 13:18:34 +0200

    Add a simple inefficient horizontal zoom

--- src/multi-timeline.js
+++ src/multi-timeline.js
@@ -1,15 +1,18 @@
-import { select as d3Select } from "d3-selection";
+import { select as d3Select, event as d3CurrentEvent } from "d3-selection";
 import { timeFormat } from "d3-time-format";
-import { timeYear } from "d3-time";
+import { timeYear, timeMonth } from "d3-time";
 import { scaleTime as d3ScaleTime, scaleBand as d3ScaleBand } from "d3-scale";
 import { axisLeft as d3AxisLeft, axisTop as d3AxisTop } from "d3-axis";
 import { transition as d3Transition } from "d3-transition";
+import { zoom as d3Zoom } from "d3-zoom";
 import "d3-selection-multi";
 
 d3Transition(); // TODO: fix this; needed so rollup does not throw out d3-selection-multi for transition...
 
 function multiFormat(date) {
-    return (timeYear(date) < date ? timeFormat("%b") : timeFormat("%Y"))(date);
+    return (timeYear(date) < date
+        ? timeMonth(date) < date ? timeFormat("%d %b") : timeFormat("%b")
+        : timeFormat("%Y"))(date);
 }
 
 function svgTranslate(left, top) {
@@ -24,15 +27,18 @@ export class MutiTimeline {
         this.skills = skills;
         this.skillNames = skillNames;
 
-        this.svg = d3Select(holder).append("svg");
         this.dims = {
             margin: { top: 35, right: 20, bottom: 25, left: 85 },
             place: { height: 20, gap: 3, textMaxSize: 15, textAdjMinSize: 10, radius: 5 },
         };
+
+        this.svg = d3Select(holder).append("svg");
+        this.redrawCounter = 0;
     }
 
     draw() {
-        this.xScale = d3ScaleTime().domain([new Date(2002, 9, 1), new Date(2017, 12, 1)]); //TODO: make dynamic
+        this.xScaleAll = d3ScaleTime().domain([new Date(2002, 9, 1), new Date(2017, 12, 1)]); //TODO: make dynamic
+        this.xScale = this.xScaleAll;
 
         this.xAxis = d3AxisTop(this.xScale).tickFormat(multiFormat);
 
@@ -116,10 +122,21 @@ export class MutiTimeline {
             .attr("width", 0)
             .attr("height", 0.8 * bandwidth);
 
+        this.zoom = d3Zoom()
+            .scaleExtent([0.8, 5])
+            .on("zoom", () => {
+                console.log(d3CurrentEvent);
+                this.xScale = d3CurrentEvent.transform.rescaleX(this.xScaleAll);
+                this.xAxis.scale(this.xScale);
+                this.redraw();
+            });
+        this.svg.call(this.zoom);
+
         this.redraw();
     }
 
     redraw() {
+        console.log("redraw " + this.redrawCounter++);
         var width =
             this.svg.node().getBoundingClientRect().width -
             this.dims.margin.left -
@@ -173,5 +190,10 @@ export class MutiTimeline {
                     "font-size": newFontSize + "px",
                 };
             });
+    }
+
+    redrawDelayed() {
+        clearTimeout(this.redrawTimer);
+        this.redrawTimer = setTimeout(this.redraw.bind(this), 100);
     }
 }
