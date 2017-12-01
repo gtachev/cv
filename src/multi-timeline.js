80640814f9f681bbfb1033a716f3ab1e6a05d1b3
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2017-12-01 16:02:45 +0200

    Remove transitions

--- src/multi-timeline.js
+++ src/multi-timeline.js
@@ -3,11 +3,8 @@ import { timeFormat } from "d3-time-format";
 import { timeYear, timeMonth } from "d3-time";
 import { scaleTime as d3ScaleTime, scaleBand as d3ScaleBand } from "d3-scale";
 import { axisLeft as d3AxisLeft, axisTop as d3AxisTop } from "d3-axis";
-import { transition as d3Transition } from "d3-transition";
 import { zoom as d3Zoom } from "d3-zoom";
 import "d3-selection-multi";
-
-d3Transition(); // TODO: fix this; needed so rollup does not throw out d3-selection-multi for transition...
 
 function multiFormat(date) {
     return (timeYear(date) < date
@@ -122,12 +119,16 @@ export class MutiTimeline {
             .attr("width", 0)
             .attr("height", 0.8 * bandwidth);
 
+        this.allRects = this.svg.selectAll("#places rect, #skills rect");
+
         this.zoom = d3Zoom()
             .scaleExtent([0.8, 5])
+            .translateExtent([[-100, 0], [100, 0]])
             .on("zoom", () => {
                 console.log(d3CurrentEvent);
                 this.xScale = d3CurrentEvent.transform.rescaleX(this.xScaleAll);
                 this.xAxis.scale(this.xScale);
+
                 this.redraw();
             });
         this.svg.call(this.zoom);
@@ -137,11 +138,15 @@ export class MutiTimeline {
 
     redraw() {
         console.log("redraw " + this.redrawCounter++);
+
         var width =
             this.svg.node().getBoundingClientRect().width -
             this.dims.margin.left -
             this.dims.margin.right;
         // var height = svg.node().getBoundingClientRect().height - this.dims.margin.top - this.dims.margin.bottom;
+
+        //TODO: fix this - part of chart is hidden after zooming and resizing
+        this.zoom.translateExtent([[-100, 0], [width + 100, 0]]);
 
         this.xScale.range([0, width]);
         this.xAxis.ticks(
@@ -153,7 +158,6 @@ export class MutiTimeline {
         var squashed = Math.pow(1 / (1 + Math.pow(Math.E, (widthPerTick - 30) / 5)), 3);
 
         this.xAxisSvg
-            .transition()
             .call(this.xAxis)
             .selectAll("text")
             //.style("text-anchor", "start")
@@ -162,15 +166,12 @@ export class MutiTimeline {
             .attr("dy", Math.sin(squashed * Math.PI / 2) * 14 - 4 + "px")
             .attr("transform", "rotate(-" + 90 * squashed + ")");
 
-        this.svg
-            .selectAll("#places rect, #skills rect")
-            .transition()
+        this.allRects
             .attr("x", d => this.xScale(d.from))
             .attr("width", d => this.xScale(d.to) - this.xScale(d.from));
 
         this.placesHolderSvg
             .selectAll("text")
-            .transition()
             .attr("x", d => (this.xScale(d.from) + this.xScale(d.to)) / 2)
             .attrs((d, e, t) => {
                 let newFontSize = Math.min(
@@ -190,10 +191,5 @@ export class MutiTimeline {
                     "font-size": newFontSize + "px",
                 };
             });
-    }
-
-    redrawDelayed() {
-        clearTimeout(this.redrawTimer);
-        this.redrawTimer = setTimeout(this.redraw.bind(this), 100);
     }
 }
